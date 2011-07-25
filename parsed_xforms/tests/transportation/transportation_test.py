a8ac14d729e0be4f270777bfadf15a3de04b7aac
Andrew Marder <andrew.ei.marder@gmail.com>
2011-07-25 14:45:17 -0700

    Added test for csv export.

--- parsed_xforms/tests/transportation/transportation_test.py
+++ parsed_xforms/tests/transportation/transportation_test.py
@@ -3,8 +3,10 @@ from parsed_xforms.models import create_xform_and_data_dictionary, \
     xform_instances
 from xform_manager.import_tools import import_instances_from_phone
 from xform_manager.models import XForm
-import json
 import os
+from parsed_xforms.views import csv_export
+from django.core.urlresolvers import reverse
+import csv
 
 
 class TestTransportationSurvey(TestCase):
@@ -13,7 +15,8 @@ class TestTransportationSurvey(TestCase):
         self.test_path = os.path.join("parsed_xforms", "tests", "transportation")
 
         xls_path = os.path.join(self.test_path, "transportation.xls")
-        create_xform_and_data_dictionary(xls_path, "transportation_2011_07_25")
+        self.id_string = "transportation_2011_07_25"
+        create_xform_and_data_dictionary(xls_path, self.id_string)
         odk_path = os.path.join(self.test_path, "odk")
         import_instances_from_phone(odk_path)
         self.assertEqual(XForm.objects.count(), 1)
@@ -55,19 +58,39 @@ class TestTransportationSurvey(TestCase):
                 "transportation": {
                     "bicycle": {
                         "frequency_to_referral_facility": "weekly"
-                        }, 
+                        },
                     "ambulance": {
                         "frequency_to_referral_facility": "daily"
-                        }, 
-                    "available_transportation_types_to_referral_facility": "ambulance bicycle", 
+                        },
+                    "available_transportation_types_to_referral_facility": "ambulance bicycle",
                     }
                 }
             }
         self.assertEqual(instance.get_dict(flat=False), expected_dict)
         expected_dict = {
-            "transportation/available_transportation_types_to_referral_facility": "ambulance bicycle", 
-            "transportation/ambulance/frequency_to_referral_facility": "daily", 
-            "transportation/bicycle/frequency_to_referral_facility": "weekly", 
-            "_xform_id_string": "transportation_2011_07_25", 
+            "transportation/available_transportation_types_to_referral_facility": "ambulance bicycle",
+            "transportation/ambulance/frequency_to_referral_facility": "daily",
+            "transportation/bicycle/frequency_to_referral_facility": "weekly",
+            "_xform_id_string": "transportation_2011_07_25",
             }
         self.assertEqual(instance.get_dict(), expected_dict)
+
+    def test_csv_export(self):
+        url = reverse(csv_export, kwargs={'id_string': self.id_string})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        actual_csv = response.content
+        actual_lines = actual_csv.split("\n")
+        actual_csv = csv.reader(actual_lines)
+        f = open(os.path.join(self.test_path, "transportation.csv"), "r")
+        expected_csv = csv.reader(f)
+        for actual_row, expected_row in zip(actual_csv, expected_csv):
+            for actual_cell, expected_cell in zip(actual_row, expected_row):
+                self.assertEqual(actual_cell, expected_cell)
+        f.close()
+
+        # TODO: Specify other questions should not be collapsed into a
+        # single column.
+
+        # TODO: Headers should be determined by the survey definition,
+        # not the data received.
