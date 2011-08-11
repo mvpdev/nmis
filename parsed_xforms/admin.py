609a454847de83ef62fe172bd5bfcbcdfff3a603
Andrew Marder <andrew.ei.marder@gmail.com>
2011-08-11 12:06:12 -0700

    Added admin site for renaming spreadsheet columns.

--- parsed_xforms/admin.py
+++ parsed_xforms/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models.data_dictionary import DataDictionary
+from .models.data_dictionary import DataDictionary, ColumnRename
 
+
+class ColumnRenameAdmin(admin.ModelAdmin):
+    list_display = ('xpath', 'column_name')
+    search_fields = ('xpath', 'column_name')
+    list_display_links = ('xpath', 'column_name')
+
+
+admin.site.register(ColumnRename, ColumnRenameAdmin)
 admin.site.register(DataDictionary)
