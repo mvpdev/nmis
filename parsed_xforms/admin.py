549ef5bc5534a53bd9b9e75cd7cc73863ab45bdb
Andrew Marder <andrew.ei.marder@gmail.com>
2011-08-11 12:21:38 -0700

    Added way to regenerate csv files through the admin.

--- parsed_xforms/admin.py
+++ parsed_xforms/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.core.management import call_command
 from .models.data_dictionary import DataDictionary, ColumnRename
 
 
@@ -6,6 +7,11 @@ class ColumnRenameAdmin(admin.ModelAdmin):
     list_display = ('xpath', 'column_name')
     search_fields = ('xpath', 'column_name')
     list_display_links = ('xpath', 'column_name')
+    actions = ['generate_csv_files']
+
+    def generate_csv_files(self, request, queryset):
+        call_command('generate_cached_export', generate_all=True)
+    generate_csv_files.short_description = "Generate CSV files."
 
 
 admin.site.register(ColumnRename, ColumnRenameAdmin)
