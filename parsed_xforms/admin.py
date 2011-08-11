bde1c17f09ecdea15a8cacc98d757d9a4cec2e9c
Andrew Marder <andrew.ei.marder@gmail.com>
2011-08-11 14:35:09 -0700

    Made everything editable from the main page.

--- parsed_xforms/admin.py
+++ parsed_xforms/admin.py
@@ -4,10 +4,15 @@ from .models.data_dictionary import DataDictionary, ColumnRename
 
 
 class ColumnRenameAdmin(admin.ModelAdmin):
-    list_display = ('xpath', 'column_name')
+    list_display = ('id', 'xpath', 'column_name')
     search_fields = ('xpath', 'column_name')
-    list_display_links = ('xpath', 'column_name')
+    list_editable = ('xpath', 'column_name')
     actions = ['generate_csv_files']
+
+    def __init__(self, *args, **kwargs):
+        # http://stackoverflow.com/questions/1618728/disable-link-to-edit-object-in-djangos-admin-display-list-only
+        super(ColumnRenameAdmin, self).__init__(*args, **kwargs)
+        self.list_display_links = (None, )
 
     def generate_csv_files(self, request, queryset):
         call_command('generate_cached_export', generate_all=True)
